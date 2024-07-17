e6fd913f9153981691e34cfe604e9fde242df554
abhinav <0341abhinav@gmail.com>
2024-07-17 23:02:42 +0530

    last version of hotel application

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose');
 
 require('dotenv').config();
-// const mongoURL='mongodb://localhost:27017/hotels';
-const mongoURL=process.env.MONGODBURL;
+const mongoURL='mongodb://localhost:27017/hotels';
+// const mongoURL=process.env.MONGODBURL;
 mongoose.connect(mongoURL,
     {
         useNewUrlParser:true, useUnifiedTopology:true 
